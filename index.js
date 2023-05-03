76cffb28b4e53958a2d695a8c464bbe2b475903d
khmamun <khmamun27@gmail.com>
2023-05-03 21:34:45 +0600

    index.js updeted

--- index.js
+++ index.js
@@ -1,18 +1,55 @@
 const express = require('express');
-const chef_data = require('./chef_data.json');
-const recipes_data = require('./recipes_data.json');
+const cors = require('cors');
+const chefsData = require('./data/chef_data.json');
+const recipesData = require('./data/recipes_data.json');
+
 const app = express();
-const port = 5000;
+const port = 3000;
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+//send all chef data
+app.get('/simply-recipes/chefs', (req, res) => {
+    res.send(chefsData);
+})
+
+//building category data from all recipe and sending it
+app.get('/simply-recipes/categories', (req, res) => {
+    const categories = [];
+    const catData = [];
+    recipesData.forEach(recipe => {
+        const recipeCat = recipe.category;
+        if(categories.indexOf(recipeCat) === -1){
+            categories.push(recipeCat);
+            catData.push({
+                category: recipe.category,
+                image: recipe.image
+            });
+        }
+    });
+    res.send(catData);
+})
+
+//send specific recipe data
+app.get('/simply-recipes/chef/:chefId', (req, res) => {
+    const id = req.params.chefId;
+    const chef = chefsData.find(chef => chef._id === id);
+    const chefRecipes = recipesData.filter(recipe => recipe.chef_id === id) || [];
+    res.send({chef, chefRecipes});
+})
+
+//send today's pick
+app.get('/simply-recipes/todays-pick', (req, res) => {
+    res.send(recipesData.slice(0 ,1));
+})
 
 
 
-app.get('/chef_data', (req, res) => {
-    res.send(chef_data);
-  })
-app.get('/recipes_data', (req, res) => {
-    res.send(recipes_data);
-  })
 
-  app.listen(port, () => {
-    console.log(`assignment10 server is running on port: ${port}`)
-  })
+app.listen(port, () => {
+  console.log(`simply recipes has been started on port ${port}`)
+})
